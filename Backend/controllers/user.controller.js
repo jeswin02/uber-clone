bd8ac89b7db13e0ec5d41f05870074857d87de98
Jeswin Mathew <jeswinabraham02@gmail.com>
2025-01-18 01:56:20 +0530

    Added user register and login endpoints

---
+++ Backend/controllers/user.controller.js
@@ -0,0 +1,54 @@
+const userModel = require("../models/user-model");
+const userService = require("../services/user.service");
+const { validationResult } = require("express-validator");
+
+module.exports.registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { fullname, email, password } = req.body;
+
+  const hashedPassword = await userModel.hashPassword(password);
+
+  const user = await userService.createUser({
+    firstname: fullname.firstname,
+    lastname: fullname.lastname,
+    email,
+    password: hashedPassword,
+  });
+  console.log("user", user);
+
+  const token = user.generateAuthToken();
+
+  res.status(201).json({ token, user });
+};
+
+module.exports.loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  console.log("email", email);
+  console.log("password", password);
+  //since we have declared password as select: false in the schema, we need to explicitly select it here to compare it with the password entered by the user during login process
+  //this time user will return the password as well
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const isMatch = await user.comparePassword(password);
+
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const token = user.generateAuthToken();
+
+  res.status(200).json({ token, user }); //send token and user details
+};
